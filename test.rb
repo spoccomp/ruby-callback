ab02d9e58a876804ce052fa72cf2518fa2ed92f9
richard trapanese <richardtrapanese@gmail.com>
2018-09-07 10:50:31 -0400

    added concept for procs

--- test.rb
+++ test.rb
@@ -1,8 +1,12 @@
 class Array
     def far_each
         for i in 0..self.size - 1
-            yield self[i]
+            yield self[i] if block_given?
         end
+        #for using a proc as a callback
+        # def far_each(callback)
+        #   callback.call(self[i])
+        # end
   end
 end 
   [1,2,3,4].far_each do |element|
@@ -13,3 +17,10 @@ end
   #  1
   #  2
   #  3
+
+
+  #for using a proc as a callback
+  # callback = Proc.new do |element|
+  #   puts element +1
+  # end
+  # [1,2,3,4].far_each(callback)
