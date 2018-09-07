e1be3b367e1e9ff78d70266a3c471f72d8022643
richard trapanese <richardtrapanese@gmail.com>
2018-09-07 08:52:57 -0400

    updated test rb file

--- test.rb
+++ test.rb
@@ -1,24 +1,8 @@
 class Array
     def far_each
-      # your implementation goes here
-      #   remember you are being passed in a block
-        
-        
         for i in 0..self.size - 1
             yield self[i]
         end
-       
-
-        # count = 0
-        # index = self.size-1
-        # #index>=0
-        # while  self.size  > count do
-            
-        #     yield self[index] 
-        #     count += 1
-        #     index -= 1
-        # end
-    
   end
 end 
   [1,2,3,4].far_each do |element|
